b4c0bb44769376961e8c80549aaac4c52e97a04a
VilPeas <contactvil@gmail.com>
2024-05-21 20:35:29 +0100

    Added wp menus to header & footer

--- code-dev/footer.php
+++ code-dev/footer.php
@@ -1,6 +1,13 @@
         <!-- -->
         <footer class="site-footer">
-
+            <div class="container">
+                <div class="copyright">
+                    <p>Copyright © <?php echo date("Y"); ?> - All rights reserved</p>
+                </div>
+                <nav class="footer-menu">
+                    <?php wp_nav_menu(array('theme_location' => 'code_dev_footer_menu', 'depth' => 1)); ?>
+                </nav>
+            </div>
         </footer>
     </div>
     <?php wp_footer(); ?>
