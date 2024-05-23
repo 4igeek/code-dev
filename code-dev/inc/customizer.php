27cc243e3d4e1969ca63668cf6d860e363d2f273
VilPeas <contactvil@gmail.com>
2024-05-23 12:19:04 +0100

    Added hero customization to customizations.php

--- code-dev/inc/customizer.php
+++ code-dev/inc/customizer.php
@@ -29,6 +29,139 @@ function code_dev_customizer( $wp_customize ){
                 'type' => 'text'
             )
         );
+    
+    // This section is for copyright information
+    $wp_customize->add_section(
+        'sec_hero',
+        array(
+            'title' => 'Hero Section'
+        )
+    );
+
+        // Hero title
+        $wp_customize->add_setting(
+            'set_hero_title',
+            array(
+                'type' => 'theme_mod',
+                'default' => 'Please add a title',
+                'sanitize_callback' => 'sanitize_text_field'
+
+            )
+        );
+
+        $wp_customize->add_control(
+            'set_hero_title',
+            array(
+                'label' => 'Hero Title',
+                'description' => 'Please type your subtitle here',
+                'section' => 'sec_hero',
+                'type' => 'text'
+            )
+        );
+
+        // Subtitle
+        $wp_customize->add_setting(
+            'set_hero_subtitle',
+            array(
+                'type' => 'theme_mod',
+                'default' => 'Please add a title',
+                'sanitize_callback' => 'sanitize_textarea_field'
+
+            )
+        );
+
+        $wp_customize->add_control(
+            'set_hero_subtitle',
+            array(
+                'label' => 'Hero Subtitle',
+                'description' => 'Please type your subtitle here',
+                'section' => 'sec_hero',
+                'type' => 'textarea'
+            )
+        );
+
+        // Button Text
+        $wp_customize->add_setting(
+            'set_hero_button_text',
+            array(
+                'type' => 'theme_mod',
+                'default' => 'Learn More',
+                'sanitize_callback' => 'sanitize_textarea_field'
+
+            )
+        );
+
+        $wp_customize->add_control(
+            'set_hero_button_text',
+            array(
+                'label' => 'Hero Button Text',
+                'description' => 'Please type your hero button text here',
+                'section' => 'sec_hero',
+                'type' => 'text'
+            )
+        );
+
+        // Button Text
+        $wp_customize->add_setting(
+            'set_hero_button_link',
+            array(
+                'type' => 'theme_mod',
+                'default' => '#',
+                'sanitize_callback' => 'esc_url_raw'
+
+            )
+        );
+
+        $wp_customize->add_control(
+            'set_hero_button_link',
+            array(
+                'label' => 'Hero Button Text',
+                'description' => 'Please type your hero button URL here',
+                'section' => 'sec_hero',
+                'type' => 'url'
+            )
+        );
+
+        // Hero Minimum Height
+        $wp_customize->add_setting(
+            'set_hero_height',
+            array(
+                'type' => 'theme_mod',
+                'default' => 800,
+                'sanitize_callback' => 'absint'
+
+            )
+        );
+
+        $wp_customize->add_control(
+            'set_hero_height',
+            array(
+                'label' => 'Hero Height',
+                'description' => 'Please type your hero height here',
+                'section' => 'sec_hero',
+                'type' => 'number'
+            )
+        );
+
+        // Hero Background
+        $wp_customize->add_setting(
+            'set_hero_background',
+            array(
+                'type' => 'theme_mod',
+                'sanitize_callback' => 'absint'
+            )
+        );
+
+        $wp_customize->add_control( new WP_Customize_Media_Control( $wp_customize,
+            'set_hero_background',
+            array(
+                'label' => 'Hero Image',
+                'section'   => 'sec_hero',
+                'mime_type' => 'image'
+            )
+        ));
+
+
 }
 
 add_action('customize_register', 'code_dev_customizer');
--- code-dev/page-home.php
+++ code-dev/page-home.php
@@ -3,8 +3,24 @@
         <div id="content" class="site-content">
             <div id="primary" class="content-area">
                 <main id="main" class="site-main">
-                    <section class="hero">
-                        Hero
+                    <?php 
+                        $hero_title = get_theme_mod('set_hero_title', 'Welcome to my site');
+                        $hero_subtitle = get_theme_mod('set_hero_subtitle', 'Add a subtitle in the theme customizer');
+                        $hero_button_text = get_theme_mod('set_hero_button_text', 'Learn More');
+                        $hero_button_link = get_theme_mod('set_hero_button_link', '#');
+                        $hero_height = get_theme_mod('set_hero_height', 800);
+                        $hero_background = wp_get_attachment_url( get_theme_mod('set_hero_background') )
+                    ?>
+                    <section class="hero" style="background-image: url('<?php echo $hero_background;?>')">
+                        <div class="overlay" style="min-height: <?php echo $hero_height; ?>px;">
+                            <div class="container">
+                                <div class="hero-items">
+                                    <h1><?php echo $hero_title; ?></h1>
+                                    <p><?php echo nl2br ($hero_subtitle); ?></p>
+                                    <a href="<?php echo $hero_button_link; ?>"><?php echo $hero_button_text; ?></a>
+                                </div>
+                            </div>
+                        </div>
                     </section>
                     <section class="home-blog">
                         <!-- Lesson 31 "Exploring the WQ Query class to loop through post categories" -->
